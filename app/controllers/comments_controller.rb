f0a111793cf7090a452eda75420268126f40368d
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-10-23 14:26:16 -0500

    Add AJAX for creating a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,13 +33,10 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
+        format.js
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
-        format.js do
-          render template: "comments/create.js.erb"
-          #Since tempate name is the same as the meetod we can also just say format.js with no block
-        end
       end
     end
   end
